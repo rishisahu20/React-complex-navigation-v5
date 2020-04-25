9b407405637f77968fa134a610b9b56ef9b79020
rishisahu94 <rishi.sahu@srijan.net>
2020-04-25 13:56:29 +0530

    folder structure change

--- App.js
+++ App.js
@@ -1,9 +1,9 @@
 import * as React from "react";
 import { Platform, StatusBar, StyleSheet, View } from "react-native";
-import RootNavigator from "./navigation/navigator";
+import RootNavigator from "./src/navigation/navigator";
 import { Provider } from "react-redux";
 import { createStore, applyMiddleware } from "redux";
-import rootReducer from "./redux/Reducers/RootReducer";
+import rootReducer from "./src/redux/Reducers/RootReducer";
 import thunk from "redux-thunk";
 
 const store = createStore(rootReducer, applyMiddleware(thunk));
